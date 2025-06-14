baf01392aa0b48e37b141652dd78d5b5a3edcc33
CrispimAraujo <dev.araujocris@gmail.com>
2025-06-14 20:15:36 -0300

    trying to make a calculator, JS not ready

--- script/script.js
+++ script/script.js
@@ -1,7 +1,26 @@
 const display = document.getElementById("display");
-const numbers = document.getElementById("button");
-const operators = document.getElementById("operators");
-const clearButton = document.getElementById("clear");
-const equalsButton = document.getElementById("equalButton");
-const dotButton = document.getElementById("dotButton");
-let currentInput = "";
+
+function appendToDisplay(input) {
+    display.value += input;
+    if (display.value.length > 20) {
+        display.value = "Input too long";
+    } else {
+        if (value == ".") {
+
+        } else if (display.value == "0") {
+            display.value = input;
+        }
+    }
+
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLastCharacter() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+}
