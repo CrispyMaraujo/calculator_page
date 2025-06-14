49f467d7e110155cd1fd1cefde00d1c82a271e0e
CrispyMaraujo <contact.crispimaraujo@gmail.com>
2025-06-13 22:44:54 -0300

    First try on my functional calculator

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const display = document.getElementById("display");
+const numbers = document.getElementById("button");
+const operators = document.getElementById("operators");
+const clearButton = document.getElementById("clear");
+const equalsButton = document.getElementById("equalButton");
+const dotButton = document.getElementById("dotButton");
+let currentInput = "";
